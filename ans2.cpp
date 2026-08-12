5ebe57d3eacce37b5ffa4068d6c5a84094c1e1c7
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-08-12 10:18:43 +0600

    Create ans2.cpp

---
+++ ans2.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int A,B,C;
+    cin>>A>>B>>C;
+    
+    if(A==B || B==C || C==A)
+        cout<<"YES\n";
+    
+    else
+        cout<<"NO\n";
+}
