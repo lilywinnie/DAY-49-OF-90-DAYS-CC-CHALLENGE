8cce298cbfdbc1c0676fe9f8ef26dd1e1ba09c34
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-08-12 10:22:12 +0600

    Implement main function to process multiple test cases

---
+++ ans3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    
+    while (T--)
+    {
+        int A,B,X,Y;
+        cin>>A>>B>>X>>Y;
+        
+        cout<< (X/A) + (Y/B) <<"\n";
+    }
+}
